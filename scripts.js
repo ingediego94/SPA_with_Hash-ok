56c96e576ac23335bcf363f47d995059b34f8a21
ingediego94 <inge.diego94@gmail.com>
2025-07-09 22:13:25 -0500

    Finaliza estruc. basica

--- scripts.js
+++ scripts.js
@@ -1,40 +1,38 @@
-
+// SPA
 // script to change of view according to SPA (Single Page Application).
 
+const container = document.getElementById('content');
 
-// *************
-const contenedor = document.getElementById('contenido');
-
-// Obtiene el nombre de la vista desde el hash
-function obtenerVistaDesdeHash() {
-  const hash = location.hash.slice(2); // Elimina "#/"
-  return hash || 'inicio'; // Vista por defecto
+// Get the name of each view from 'hash'.
+function getViewFromHash() {
+  const hash = location.hash.slice(2); // Deleting "#/"
+  return hash || 'inicio'; // Default view
 }
 
-// Carga dinámicamente la vista HTML
-function cargarVista(nombre) {
-  fetch(`vistas/${nombre}.html`)
+// Dynamically loads the HTML view.
+function loadView(nameOfView) {
+  fetch(`vistas/${nameOfView}.html`)
     .then(res => {
-      if (!res.ok) throw new Error(`No se pudo cargar la vista: ${nombre}`);
+      if (!res.ok) throw new Error(`No se pudo cargar la vista: ${nameOfView}`);
       return res.text();
     })
     .then(html => {
-      contenedor.innerHTML = html;
+      container.innerHTML = html;
     })
     .catch(err => {
-      contenedor.innerHTML = `<p>Error al cargar la vista "${nombre}"</p>`;
+      container.innerHTML = `<p>Error al cargar la vista "${nameOfView}"</p>`;
       console.error(err);
     });
 }
 
-// Escucha cambios en el hash (ej. usuario usa el botón atrás/adelante)
+// It listening changes in the hash (the user push back / foward )
 window.addEventListener('hashchange', () => {
-  const vista = obtenerVistaDesdeHash();
-  cargarVista(vista);
+  const view = getViewFromHash();
+  loadView(view);
 });
 
-// Al cargar la página por primera vez
+// When the page loads for first time.
 document.addEventListener('DOMContentLoaded', () => {
-  const vista = obtenerVistaDesdeHash();
-  cargarVista(vista);
+  const view = getViewFromHash();
+  loadView(view);
 });
